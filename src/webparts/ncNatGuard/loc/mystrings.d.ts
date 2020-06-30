f009599c1702fa1240307e43a0799853a908f610
Isabella Wang <57301000+isabellawwang@users.noreply.github.com>
2020-06-30 08:52:38 -0400

    first commit (PR #1)

---
+++ src/webparts/ncNatGuard/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INcNatGuardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NcNatGuardWebPartStrings' {
+  const strings: INcNatGuardWebPartStrings;
+  export = strings;
+}
